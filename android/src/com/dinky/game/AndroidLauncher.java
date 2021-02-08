3b569e039f8653ce1bfe7bd553017f82d07bf12b
TobiasElchi <tschaeffer@freenet.de>
2021-02-08 22:52:09 +0100

    Initial commit

---
+++ android/src/com/dinky/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dinky.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dinky.game.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
